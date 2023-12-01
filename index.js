48ca522efe8c578c668e222572c3b2949ec6d947
Ramesh BG <78341158+rameshbg-openbank@users.noreply.github.com>
2023-12-01 15:01:04 +0530

    added default sample app route

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 var http = require("http");
 var express = require("express");
 var RED = require("node-red");
-
 // Create an Express app
 var app = express();
 
 // Add a simple route for static content served from 'public'
 app.use("/", express.static("public"));
+
+app.get("/", function (req, res) {
+  res.send(
+    "Hello . This is a sample Node App . Please go to /red to see API flows"
+  );
+});
 
 // Create a server
 var server = http.createServer(app);
